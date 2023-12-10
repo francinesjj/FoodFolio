48e37e9fc0fb8041a2e54ea1c1db6d6e81be86a5
Francine San Jose <franzsjj@gmail.com>
2023-12-10 16:32:32 +0800

    commit

---
+++ AAAA.kt
@@ -0,0 +1,11 @@
+package com.example.appdevalt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AAAA : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_aaaa)
+    }
+}
